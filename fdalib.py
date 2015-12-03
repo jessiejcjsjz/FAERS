cf7b6c0133ad153a1d160d552535a707b49cabdb
mlbernauer <mlbernauer@gmail.com>
2015-12-03 16:14:18 -0700

    update

--- fdalib.py
+++ fdalib.py
@@ -1,6 +1,24 @@
-def proportional_reporting_ratio(drug, event, db):
-    n = int(db.execute("SELECT COUNT(*) FROM EVENT WHERE DRUGNAME LIKE '%s' AND PT LIKE '%s'" % (drug, event)).fetchone()[0])
-    m = float(db.execute("SELECT COUNT(*) FROM EVENT WHERE DRUGNAME LIKE '%s' AND PT NOT LIKE '%s'" % (drug, event)).fetchone()[0])
-    p = int(db.execute("SELECT COUNT(*) FROM EVENT WHERE DRUGNAME NOT LIKE '%s' AND PT LIKE '%s'" % (drug, event)).fetchone()[0])
-    q = float(db.execute("SELECT COUNT(*) FROM EVENT WHERE DRUGNAME NOT LIKE '%s' AND PT NOT LIKE '%s'" % (drug, event)).fetchone()[0])
-    return (n/m) / (p/q)
+import sqlite3
+import drugstandards as drugs
+
+class FAERS:
+    def connect(self, filename):
+        """ This method is used to establish a connection 
+            with an existing FAERS database.
+        """
+        self.conn = sqlite3.connect(filename)
+        
+    def proportional_reporting_ratio(self, drug, event):
+        """ This method wil return the proportional-reporting-ratio (PRR)
+            for a given drug-reaction pair.
+        """
+        n = int(self.conn.execute("SELECT COUNT(*) FROM EVENT WHERE DRUGNAME LIKE '%s' AND PT LIKE '%s'" % (drug, event)).fetchone()[0])
+        m = float(self.conn.execute("SELECT COUNT(*) FROM EVENT WHERE DRUGNAME LIKE '%s' AND PT NOT LIKE '%s'" % (drug, event)).fetchone()[0])
+        p = int(self.conn.execute("SELECT COUNT(*) FROM EVENT WHERE DRUGNAME NOT LIKE '%s' AND PT LIKE '%s'" % (drug, event)).fetchone()[0])
+        q = float(self.conn.execute("SELECT COUNT(*) FROM EVENT WHERE DRUGNAME NOT LIKE '%s' AND PT NOT LIKE '%s'" % (drug, event)).fetchone()[0])
+        return (n/m) / (p/q)
+
+    def common_events(self, drugname):
+        drugname = drugs.standardize([drugname])[0]
+        return(self.conn.execute("SELECT DRUGNAME, PT, COUNT(*) AS COUNT FROM EVENT WHERE DRUGNAME = '%s' GROUP BY DRUGNAME, PT ORDER BY COUNT" % drugname.upper()).fetchall())
+
