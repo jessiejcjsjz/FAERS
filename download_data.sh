f99019602dc733b914cc8f95fdfae544ce9e82e0
mlbernauer <mlbernauer@gmail.com>
2015-12-09 22:53:39 -0700

    update

--- download_data.sh
+++ download_data.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-# Check if data directory exists, create if not.
-if [ ! -d "data/" ]; then
-  mkdir "data/"
-fi
-
-# Fetch links to data.
-python get_data_links.py > tmp.txt
-
-# Only download files that are not in data/
-if [ "$(ls data/)" ]; then
-  echo "Directory is not empty"
-  CURRENT_FILES=$(ls data/)
-  NEW_FILES=$(grep -v -F "${CURRENT_FILES}" tmp.txt)
-else
-  echo "Directory is empty; using tmp.txt"
-  NEW_FILES=$(cat tmp.txt)  
-fi
-
-# wget the new files
-echo "${NEW_FILES}" | xargs -I % wget -P data/ %
-rm tmp.txt
+cat links.txt | xargs -I % wget -P data/ %
--- import_faers_data.sh
+++ import_faers_data.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
 
 # Create database schema.
-DRUG="CREATE TABLE DRUG (ISR INT,DRUG_SEQ INT,ROLE_CODE TEXT,DRUGNAME TEXT,VAL_VBM INT,ROUTE TEXT,DOSE_VBM TEXT,DECHAL TEXT,RECHAL TEXT,LOT_NUM CHAR,EXPT_DT INT,NDA_NUM TEXT);" 
-DEMO="CREATE TABLE DEMO (ISR INT,CASE_NUM INT,IF_CODE TEXT,FOIL_SEQ TEXT,IMAGE TEXT,EVENT_DT INT,MFR_DT INT,FDA_DT INT,REPT_CODE TEXT,MFR_NUM TEXT,MFR_SNDR TEXT,AGE REAL,AGE_CODE TEXT,GNDR_CODE TEXT,E_SUB TEXT,WT REAL,WT_CODE TEXT,REPT_DT INT,OCCP_CODE TEXT,DEATH_DT INT,TO_MFR TEXT,ONFID TEXT,REPORTER_COUNTRY TEXT);"
-INDI="CREATE TABLE INDI (ISR INT,DRUG_SEQ INT,INDI_PT TEXT);"
-OUTC="CREATE TABLE OUTC (ISR INT,OUT_CODE TEXT);"
-REAC="CREATE TABLE REAC (ISR INT,PT TEXT);"
-RPSR="CREATE TABLE RPSR (ISR INT,RPSR_CODE TEXT);"
-THER="CREATE TABLE THER (ISR INT,DRUG_SEQ INT,START_DT INT,END_DT INT,DUR INT,DUR_CODE TEXT);"
-
-# Remove existing database
-if [ -e "faers.db" ]; then
-  echo "Do you want to overwrite existing database? y/[n]"
-  read response
-  if [[ "$response" == "y" ]]; then
-    echo "Removing database"
-    rm faers.db
-  elif [[ "$response" == "n" ]]; then
-    echo "Exiting..."
-    exit
-  else
-    "Exiting..."
-    exit
-  fi  
-fi
+DRUG="CREATE TABLE drugs (PRIMARYID INT, CASEID INT, DRUG_SEQ INT, ROLE_COD TEXT, DRUGNAME TEXT, VAL_VBM TEXT, ROUTE TEXT, DOSE_VBM TEXT, CUM_DOSE_CHR TEXT, CUM_DOS_UNIT TEXT, DECHAL TEXT, RECHAL TEXT, LOT_NUM TEXT, EXP_DT INT, NDA_NUM INT, DOSE_AMT TEXT, DOSE_UNIT TEXT, DOSE_FORM TEXT, DOSE_FREQ TEXT);"
+DEMO="CREATE TABLE demos (PRIMARYID INT, CASEID INT, CASE_VERSION INT, IF_CODE TEXT, EVENT_DT INT, MFR_DT INT, INIT_FDA_DATE INT, FDA_DT INT, REPT_COD TEXT, MFR_NUM TEXT, MFR_SNDR TEXT, AGE INT, AGE_COD TEXT, GNDR_COD TEXT, E_SUB TEXT, WT INT, WT_COD TEXT, REPT_DT INT, TO_MFR TEXT, OCCP_COD TEXT, REPORTER_COUNTRY TEXT, OCCR_COUNTRY);"
+INDI="CREATE TABLE indis (PRIMARYID INT, CASEID INT, INDI_DRUG_SEQ INT, INDI_PT TEXT);"
+OUTC="CREATE TABLE outcs (PRIMARYID INT, CASEID INT, OUTC_COD TEXT);"
+REAC="CREATE TABLE reacs (PRIMARYID INT, CASEID INT, PT TEXT);"
+RPSR="CREATE TABLE rpsrs (PRIMARYID INT, CASEID INT, RPSR_COD TEXT);"
+THER="CREATE TABLE thers (PRIMARYID INT, CASEID INT, DSG_DRUG_SEQ INT, START_DT INT, END_DT INT, DUR INT, DUR_COD TEXT);"
 
 # Setup database schema
+echo "Setting up schema..."
 sqlite3 faers.db "${DRUG}"
 sqlite3 faers.db "${DEMO}"
 sqlite3 faers.db "${INDI}"
@@ -38,34 +23,61 @@ chmod +w faers.db
 
 # Unzip and import data into sqlite database.
 for filename in data/*.zip; do
-  echo "`date`: beginning load for $filename"
-  unzip -o $filename -d data/  
-  find -name DRUG*TXT | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' DRUG"
-  find -name DEMO*TXT | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' DEMO"
-  find -name INDI*TXT | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' INDI"
-  find -name OUTC*TXT | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' OUTC"
-  find -name REAC*TXT | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' REAC"
-  find -name RPSR*TXT | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' RPSR"
-  find -name THER*TXT | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' THER"
-  rm -rf ./data/ascii
+  unzip -o $filename -d data/
+  echo "`date`: begninning import for $filename"
+  echo "Importing DRUG table..."
+  find data/ -iname DRUG*txt | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' drugs"
+  echo "Importing DEMO table..."
+  find data/ -iname DEMO*txt | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' demos"
+  echo "Importing INDI table..."
+  find data/ -iname INDI*txt | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' indis"
+  echo "Importing OUTC table..."
+  find data/ -iname OUTC*txt | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' outcs"
+  echo "Importing REAC table..."
+  find data/ -iname REAC*txt | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' reacs"
+  echo "Importing RPSR table..."
+  find data/ -iname RPSR*txt | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' rpsrs"
+  echo "Importing THER table..."
+  find data/ -iname THER*txt | xargs -I % sqlite3 faers.db -separator $ ".import '"%"' thers"
+
+  # Clean up files.
+  find data/ -name '*txt' -exec rm {} \;
+  find data/ -name '*pdf' -exec rm {} \;
+  find data/ -name '*doc' -exec rm {} \;
+  rm -rf ./data/sgml
+  rm -rf ./data/sqml
+  rm -rf ./data/as*
+  rm -rf ./data/ASCII
 done;
 
 # Delete header rows.
-sqlite3 faers.db "DELETE FROM DRUG WHERE ISR = 'ISR'"
-sqlite3 faers.db "DELETE FROM DEMO WHERE ISR = 'ISR'"
-sqlite3 faers.db "DELETE FROM INDI WHERE ISR = 'ISR'"
-sqlite3 faers.db "DELETE FROM OUTC WHERE ISR = 'ISR'"
-sqlite3 faers.db "DELETE FROM REAC WHERE ISR = 'ISR'"
-sqlite3 faers.db "DELETE FROM RPSR WHERE ISR = 'ISR'"
-sqlite3 faers.db "DELETE FROM THER WHERE ISR = 'ISR'"
+sqlite3 faers.db "DELETE FROM drugs WHERE PRIMARYID = 'primaryid'"
+sqlite3 faers.db "DELETE FROM demos WHERE PRIMARYID = 'primaryid'"
+sqlite3 faers.db "DELETE FROM indis WHERE PRIMARYID = 'primaryid'"
+sqlite3 faers.db "DELETE FROM outcs WHERE PRIMARYID = 'primaryid'"
+sqlite3 faers.db "DELETE FROM reacs WHERE PRIMARYID = 'primaryid'"
+sqlite3 faers.db "DELETE FROM rpsrs WHERE PRIMARYID = 'primaryid'"
+sqlite3 faers.db "DELETE FROM thers WHERE PRIMARYID = 'primaryid'"
 
-# Create indices on DRUG and REAC to speed up merging.
-sqlite3 faers.db "CREATE INDEX drug_idx ON DRUG (ISR, DRUGNAME)"
-sqlite3 faers.db "CREATE INDEX reac_idx ON REAC (ISR, PT)"
-sqlite3 faers.db "CREATE INDEX demo_idx ON DEMO (REPORTER_COUNTRY, EVENT_DT)"
+# Remove duplicate rows from tables
+sqlite3 faers.db "CREATE TABLE DRUG AS SELECT * FROM drugs GROUP BY PRIMARYID, DRUG_SEQ"
+sqlite3 faers.db "CREATE TABLE DEMO AS SELECT * FROM demos GROUP BY PRIMARYID"
+sqlite3 faers.db "CREATE TABLE INDI AS SELECT * FROM indis GROUP BY PRIMARYID, INDI_DRUG_SEQ, INDI_PT"
+sqlite3 faers.db "CREATE TABLE OUTC AS SELECT * FROM outcs GROUP BY PRIMARYID, OUTC_COD"
+sqlite3 faers.db "CREATE TABLE REAC AS SELECT * FROM reacs GROUP BY PRIMARYID, PT"
+sqlite3 faers.db "CREATE TABLE RPSR AS SELECT * FROM rpsrs GROUP BY PRIMARYID, RPSR_COD"
+sqlite3 faers.db "CREATE TABLE THER AS SELECT * FROM thers GROUP BY PRIMARYID, DSG_DRUG_SEQ"
 
-# Clean up files.
-find -name *TXT -exec rm {} \;
-rm -rf ./data/sgml
-rm -rf ./data/sqml
-rm ./data/README.doc
+# Remove old tables
+sqlite3 faers.db "DROP TABLE drugs"
+sqlite3 faers.db "DROP TABLE demos"
+sqlite3 faers.db "DROP TABLE indis"
+sqlite3 faers.db "DROP TABLE outcs"
+sqlite3 faers.db "DROP TABLE reacs"
+sqlite3 faers.db "DROP TABLE rpsrs"
+sqlite3 faers.db "DROP TABLE thers"
+
+# Create indisces on DRUG and REAC to speed up merging.
+sqlite3 faers.db "CREATE INDEX drugs_idx ON DRUG (PRIMARYID, DRUGNAME)"
+sqlite3 faers.db "CREATE INDEX reacs_idx ON REAC (PRIMARYID, PT)"
+sqlite3 faers.db "CREATE INDEX demos_idx ON DEMO (REPORTER_COUNTRY, EVENT_DT)"
